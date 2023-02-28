0d7b1ab31e3584e1682d37be04de0a57db71a8ec
hypercubist <58693701+hypercubist@users.noreply.github.com>
2023-02-28 14:11:32 +0900

    프로젝트 초기화 start.spring.io

---
+++ src/main/java/jpabook/kyhspringbootjpa1/KyhSpringbootJpa1Application.java
@@ -0,0 +1,13 @@
+package jpabook.kyhspringbootjpa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyhSpringbootJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyhSpringbootJpa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/jpabook/kyhspringbootjpa1/KyhSpringbootJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.kyhspringbootjpa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyhSpringbootJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
